4946fc505122bf2a804baa6e26e68c37ad91e915
Laura Gomez <laura@elenagoto.ch>
2020-11-04 18:09:11 +0100

    Update rails version to 5.2 to use gem 'google_sign_in'

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ git_source(:github) do |repo_name|
 end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.7'
+gem 'rails', '~> 5.2'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
@@ -53,3 +53,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+
+# For raisl 5.2
+gem 'bootsnap', require: false
+
+# For signin and login with Google
+gem 'google_sign_in'
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  root to: 'home#index'
-
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :users
-  resources :posts
-
-  get 'search', to: 'posts#index', as: 'search'
-  get 'tags/:tag', to: 'posts#index', as: :tag
-
-  # Login and Sign up routes
-  get 'log_in', to: 'sessions#new', as: 'login'
-  get 'sign_up', to: 'users#new', as: 'signup'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
