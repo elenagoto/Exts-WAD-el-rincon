7a7ae205ec3f59d9030ddec8d9a3409f4a3c1f44
Laura Gomez <laura@elenagoto.ch>
2020-11-04 20:25:30 +0100

    Add again erased routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  resources :posts
+
+  get 'search', to: 'posts#index', as: 'search'
+  get 'tags/:tag', to: 'posts#index', as: :tag
+
+  # Login and Sign up routes
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'sign_up', to: 'users#new', as: 'signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
