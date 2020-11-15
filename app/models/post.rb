5317eaa93e62808582b9750ee00932fe2af65c8e
Laura Gomez <laura@elenagoto.ch>
2020-11-15 21:10:30 +0100

    Correct search to lower also title

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,7 +14,7 @@ class Post < ApplicationRecord
 
   # Scopes
   scope :most_recent, -> { order(created_at: :desc).limit(6) }
-  scope :title_contains, ->(term) { where('title LIKE ?', "%#{term}%") }
+  scope :title_contains, ->(term) { where('lower(title) LIKE ?', "%#{term}%") }
   scope :tags_contain, ->(term) { joins(:tags).where('tags.name LIKE ?', "%#{term}%") }
   scope :search, ->(search_term) { title_contains(search_term) | tags_contain(search_term) }
   scope :tagged_with, ->(name) { Tag.find_by!(name: name).posts }
