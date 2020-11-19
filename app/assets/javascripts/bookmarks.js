9b39d952657155c5ea4ecb494da45aeb8eded6f7
Laura Gomez <laura@elenagoto.ch>
2020-11-19 17:54:41 +0100

    Make bookmarks work with asynchronous request

--- app/assets/javascripts/bookmarks.js
+++ app/assets/javascripts/bookmarks.js
@@ -1,2 +1,27 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var Bookmarks;
+
+Bookmarks = {};
+
+// Remove bookmark function
+Bookmarks.removePost = function(postId) {
+  post = document.getElementById('post-' + postId);
+
+  if (post) {
+    post.remove();
+  } else {
+    buttonDelete = document.querySelector('#form-post-' + postId + ' .favorite-delete');
+    buttonAdd = document.querySelector('#form-post-' + postId + ' .favorite-add');
+    buttonDelete.classList.add('invisible') ;
+    buttonAdd.classList.remove('invisible');
+  }
+}
+
+// Add bookmark function
+Bookmarks.addPost = function(postId) {
+  buttonDelete = document.querySelector('#form-post-' + postId + ' .favorite-delete');
+  buttonAdd = document.querySelector('#form-post-' + postId + ' .favorite-add');
+  buttonDelete.classList.remove('invisible') ;
+  buttonAdd.classList.add('invisible');
+}
--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,17 +2,23 @@ class BookmarksController < ApplicationController
   before_action :ensure_authenticated
 
   def create
-    post = Post.find_by_id(params[:post_id])
-    logger.info("This is the post: #{post.title}")
-    current_user.bookmarked_posts << post
-    redirect_to root_path
+    @post = Post.find_by_id(params[:post_id])
+    logger.info("This is the post: #{@post.title}")
+    current_user.bookmarked_posts << @post
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
   end
 
   def destroy
-    post = Post.find_by_id(params[:id])
-    logger.info("This is the post: #{params[:id]}")
-    logger.info("This is the post: #{post.title}")
-    current_user.bookmarked_posts.delete(post)
-    redirect_to root_path
+    @post = Post.find_by_id(params[:id])
+    current_user.bookmarked_posts.delete(@post)
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
   end
 end
